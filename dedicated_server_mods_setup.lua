90c56e8eb027591689811d470ed83e540c4a413e
chientrm <chientrm@gmail.com>
2024-09-07 00:50:59 +0000

    f

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -1,6 +1,7 @@
-ServerModSetup("2039181790") -- uncomp
-ServerModSetup("378160973") -- global positions
-ServerModSetup("2189004162") -- insights
-ServerModSetup("1185229307") -- epic health bar
-ServerModSetup("2823530744") -- extra slot
-ServerModSetup("1289779251") -- cherry forest
+-- ServerModSetup("2039181790") -- uncomp
+-- ServerModSetup("378160973") -- global positions
+-- ServerModSetup("2189004162") -- insights
+-- ServerModSetup("1185229307") -- epic health bar
+-- ServerModSetup("2823530744") -- extra slot
+-- ServerModSetup("1289779251") -- cherry forest
+ServerModSetup("3322803908") -- above the cloud
