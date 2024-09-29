6cd11fd7af21bb4ecf99888c534f6c2504494e2a
Ubuntu <ubuntu@ip-172-31-44-187.ap-southeast-1.compute.internal>
2024-09-29 19:45:43 +0000

    tropical

--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -1,7 +1,9 @@
 -- ServerModSetup("2039181790") -- uncomp
 -- ServerModSetup("378160973") -- global positions
 -- ServerModSetup("2189004162") -- insights
--- ServerModSetup("1185229307") -- epic health bar
+ServerModSetup("1185229307") -- epic health bar
+ServerModSetup("1505270912") -- tropical
+ServerModSetup("1595631294") -- smart mini signs
 -- ServerModSetup("2823530744") -- extra slot
 -- ServerModSetup("1289779251") -- cherry forest
-ServerModSetup("3322803908") -- above the cloud
+-- ServerModSetup("3322803908") -- above the cloud
