a636cd01fb6220a58ab4708c809927973e49afac
Ubuntu <ubuntu@ip-172-31-36-204.ap-southeast-1.compute.internal>
2024-10-03 04:04:00 +0000

    f

--- beta_mods.lua
+++
@@ -1,3 +0,0 @@
--- ServerModSetup("378160973") -- global positions
--- ServerModSetup("2189004162") -- insights
--- ServerModSetup("1185229307") -- epic health bar
--- dedicated_server_mods_setup.lua
+++ dedicated_server_mods_setup.lua
@@ -2,8 +2,10 @@
 -- ServerModSetup("378160973") -- global positions
 -- ServerModSetup("2189004162") -- insights
 ServerModSetup("1185229307") -- epic health bar
-ServerModSetup("1505270912") -- tropical
+-- ServerModSetup("1505270912") -- tropical
 ServerModSetup("1595631294") -- smart mini signs
+ServerModSetup("1467214795") -- island adventure
+ServerModSetup("1378549454") -- gemcore
 -- ServerModSetup("2823530744") -- extra slot
 -- ServerModSetup("1289779251") -- cherry forest
 -- ServerModSetup("3322803908") -- above the cloud
